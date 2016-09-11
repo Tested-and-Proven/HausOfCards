d7d88ae7130625b109bc906dfc4c1dd070ab29cf
Dion <Dion.Fulwood@gmail.com>
2016-09-11 04:12:01 -0700

    Initial setup of project structure and configuration of webpack

---
+++ client/app.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+    constructor(props){
+      super(props);
+    }
+    render () {
+      return (
+          <div>
+            <h1>We In Here!!</h1>
+          </div>
+        );
+    }
+}
+
+export default App;
