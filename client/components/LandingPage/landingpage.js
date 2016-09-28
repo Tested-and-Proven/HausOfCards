9c67d23fc3030585d999c3fefc38f631977c7725
Dion <Dion.Fulwood@gmail.com>
2016-09-27 19:44:18 -0700

    Creates Landing page component

---
+++ client/components/LandingPage/landingpage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+
+class LandingPage extends Component {
+constructor() {
+  super()
+}
+render() {
+  return(
+      <main>
+        <h1>Welcome To Haus of Cards</h1>
+        <h3>Haus of Cards allows you to create decks of flash cards for your studying needs! It's easy to get started, Just Login and begin creating your decks!</h3>
+      </main>
+    )
+}
+}
+export default LandingPage
