f06783461defd5d0abc290b3312a1745b9dbd145
Dion <Dion.Fulwood@gmail.com>
2016-09-27 20:39:13 -0700

    Removes plugin from babelrc and package.json. Modifies React Router for landingpage, loginpage and register page. Adds Redux to server.js

---
+++ client/components/LandingView/landingpage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+
+class LandingPage extends Component {
+constructor() {
+  super()
+}
+render() {
+  return(
+      <main>
+        <header><h1>Welcome To Haus of Cards</h1></header>
+        <section><h3>Haus of Cards allows you to create decks of flash cards for your studying needs! It's easy to get started, Just Login and begin creating your decks!</h3></section>
+      </main>
+    )
+}
+}
+export default LandingPage
